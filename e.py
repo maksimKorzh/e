b0f0784b535bbd655c1bb1d0ec16f5814e6fe6c2
Code Monkey King <freesoft.for.people@gmail.com>
2025-01-04 07:27:38 +0200

    stripped down to 2040 bytes

--- e.py
+++ e.py
@@ -1,17 +1,18 @@
 #!/bin/python3
-import curses as u,os;os.environ['ESCDELAY']='10';s=u.initscr()
-s.nodelay(1);u.noecho();u.raw();s.keypad(1);u.start_color();u.use_default_colors()
-v={'b':[],'r':0,'c':0,'i':0,'m':'N','x':0,'y':0,'R':0,'C':0,'s':s,'f':'out.txt','u':u,
+import curses as u;s=u.initscr()
+s.nodelay(1);u.noecho();u.raw();s.keypad(1);
+v={'b':[],'r':0,'c':0,'i':0,'m':'N','x':0,'y':0,'R':0,'C':0,'s':s,'f':'o.txt','u':u,
 104:'if c>0:c-=1',108:'if c<len(b[r]):c+=1',107:'if r!=0:r-=1',106:'if r<len(b)-1:r+=1',
 100:'if len(b):del b[r];r=r if r<len(b) else r-1 if r-1 >= 0 else 0',36:'c=len(b[r])',
 48:'c=0',21:'r=r-5 if r-5>0 else 0',4:'r=r+5 if r+5<len(b)-1 else len(b)-1',105:'m="I"',
+120:'if len(b[r]):del b[r][c]\nif c and c>len(b[r])-1:c=len(b[r])-1',
 't':['if i!=((i)&0x1f) and i<128:b[r].insert(c,i);c+=1\nif i==263:\n if c==0 and r!=0:',
 'l=b[r][c:];del b[r];r-=1;c=len(b[r]);b[r]+=l\n elif c:c-=1;del b[r][c]\nif i==10:',
 'l=b[r][c:];b[r]=b[r][:c];r+=1;c=0;b.insert(r,[]+l)'],'p':['R,C=s.getmaxyx();R-=1\n',
 'if r<y:y=r\nif r>=y+R:y=r-R+1\nif c<x:x=c\nif c>=x+C:x=c-C+1\nfor Y in range(R):\n ',
 'for X in range(C):\n  try:s.addch(Y,X,b[Y+y][X+x])\n  except:pass\n s.clrtoeol()\n ',
 'try:s.addch(10)\n except:pass\nu.curs_set(0);s.clrtoeol();s.addstr(R,0,m+" -- "+f+" ',
-'-- Row: "+str(r)+", Col: "+str(c));s.move(r-y,c-x);u.curs_set(1);s.refresh();i=-1'],
+'-- Row: "+str(r+1)+", Col: "+str(c+1));s.move(r-y,c-x);u.curs_set(1);s.refresh();i=-1'],
 'a':'if not len(b):b=[[]]\nif c>len(b[r]):c=len(b[r])','z':['try:\n with open(f) as i:',
 '\n  c=i.read().split("\\n");c=c[:-1] if len(c)>1 else c\n  for i in c:b.append([ord(c) ',
 'for c in i]);r=len(b)-1;c=len(b[r])\nexcept:b.append([])'],'w':['d=""\nfor l in b:',
